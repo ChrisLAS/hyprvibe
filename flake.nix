04f1b536e1abf7924fa5ef9c37554637ba70e5e4
ChrisLAS <chris@jupiterbroadcasting.com>
2026-08-30 07:04:34 -0700

    dms: pin SyncShell v0.1.0 release

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
     dankcalendar.url = "github:AvengeMedia/dankcalendar";
     dankcalendar.inputs.nixpkgs.follows = "nixpkgs";
 
-    syncshell-dms.url = "github:ChrisLAS/syncshell-dms/a60a447";
+    syncshell-dms.url = "github:ChrisLAS/syncshell-dms/v0.1.0";
     syncshell-dms.inputs.nixpkgs.follows = "nixpkgs";
 
     sops-nix.url = "github:Mic92/sops-nix";
