a24a463bd42f5f14cd4be08d40a09407e3c41db0
Chris Fisher <chrisf@example.com>
2026-09-25 04:14:20 -0700

    build: pin reviewed eleven-item owner policy

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     # Shared client implementation; host policy remains owned by this repository.
     colony-client = {
-      url = "git+https://github.com/ChrisLAS/nomad-nixos?ref=main&rev=d11abee94d5e3bf57d98f48a2b3c5b8fa8cdda75";
+      url = "git+https://github.com/ChrisLAS/nomad-nixos?ref=main&rev=36a1e90265d17cf4521da080b17326c420f1a478";
       flake = false;
     };
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
